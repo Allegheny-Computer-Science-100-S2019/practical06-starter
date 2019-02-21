95daf11dcffa8a0731702cd968c390861fb03335
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-02-21 14:25:15 -0500

    Starter files for practical 6

---
+++ src/main/java/practicalsix/RollDice.java
@@ -0,0 +1,25 @@
+package practicalsix;
+
+public class RollDice {
+
+  private static final int MAX = 6;
+
+  public static void main(String[] args) {
+    // Step One: Declare two int variables to store face value of each die
+
+    // Step Two: Simulate each die roll
+
+    // Step Three: Print out the face value of each die
+
+    // Step Four: Compute and output the sum of each die's face value
+
+    // Step Five: Take the first die's value to the power of the second
+    // die's value and output the result
+
+    // Step Six: Find and output the maximum of the two dice values
+
+    // Step Seven: Find and output the minimum of the two dice values
+
+  }
+
+}
